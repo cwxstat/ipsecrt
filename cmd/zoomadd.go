7bba78699715432939334e68ec44f03f854b8412
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-12-03 15:52:55 -0500

    update-automated-save

--- cmd/zoomadd.go
+++ cmd/zoomadd.go
@@ -27,14 +27,17 @@ to quickly create a Cobra application.`,
 		if err != nil {
 			fmt.Println(err)
 		}
+		count := 0
 		for _, v := range cmds {
 			c := strings.Fields(v)
 			_, err := route.Run(c[0], c[1:]...)
 			if err != nil {
 				fmt.Println(err)
+				continue
 			}
+			count++
 		}
-
+		fmt.Printf("Added %d zoom routes\n", count)
 	},
 }
 
