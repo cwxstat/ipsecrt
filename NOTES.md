2a241be976e245345911389d71d808dcd5247ddd
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-12-03 18:27:55 -0500

    update-automated-save

--- NOTES.md
+++ NOTES.md
@@ -9,9 +9,18 @@ cobra-cli add version
 
 
 
-
+netstat -nr -f inet
 
 netstat -nr -f inet
 nettop -m route
 
 https://assets.zoom.us/docs/ipranges/Zoom.txt
+
+
+# Other IPs
+https://stackoverflow.com/questions/9342782/is-there-a-way-to-get-all-ip-addresses-of-youtube-to-block-it-with-windows-firew
+
+whois -h whois.radb.net -- '-i origin AS15169' | grep ^route
+
+whois -h whois.radb.net -- '-i origin AS36459' | grep ^route
+36459
--- cmd/info.go
+++ cmd/info.go
@@ -1,5 +1,6 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
 */
 package cmd
 
@@ -9,9 +10,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// versionCmd represents the version command
-var versionCmd = &cobra.Command{
-	Use:   "version",
+// infoCmd represents the info command
+var infoCmd = &cobra.Command{
+	Use:   "info",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -20,20 +21,28 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version 0.0.2")
+		msg := `
+These are common commands:
+   
+   netstat -nr -f inet
+   nettop -m route
+
+
+`
+		fmt.Println(msg)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(infoCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// infoCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// infoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
