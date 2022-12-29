c65ed4cb78e2ff3bd82b721b7d458bff671a1e13
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-12-29 09:39:56 -0500

    update-automated-save

--- cmd/version.go
+++ cmd/version.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/cwxstat/ipsecrt/constants"
 
 	"github.com/spf13/cobra"
 )
@@ -20,7 +21,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version 0.0.0")
+		fmt.Println("version", constants.VERSION)
 	},
 }
 
